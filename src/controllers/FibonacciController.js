a3dc75096c7de7ff60b842169550797a6b2d1337
Damian Ruiz <damian.ruiz@truehome.com.mx>
2021-05-26 19:53:12 -0500

    feat(contollers): add router and controller for fibonacci

--- src/controllers/FibonacciController.js
+++ src/controllers/FibonacciController.js
@@ -1,9 +1,24 @@
 'use strict';
 
 /**
-* @desc GET the fibonacci number
+* @desc get the fibonacci number
+* @params {Number}
+*/
+const fibonacciIndex = index => {
+  let fib = [0, 1];
+  for (let i = 2; i <= index; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[index];
+}
+
+/**
+* @desc GET for return the fibonacci number
 * @params {Object} {Object}
 */
 exports.getFibonacci = (req, response) => {
-  console.log("req.params.index", req.params.index);
+  response.status(200).json({
+    success: true,
+    fibonacci: fibonacciIndex(req.params.index)
+  });
 }
